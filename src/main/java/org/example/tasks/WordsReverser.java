60b5b0fe5695412e7fed7ba21953b0d121c089d5
Bi4eps <smovzhmykyta@gmail.com>
2024-07-21 13:40:47 +0300

    ref: move class WordsReverser from package "tools" to package "tasks"

---
+++ src/main/java/org/example/tasks/WordsReverser.java
@@ -0,0 +1,22 @@
+package org.example.tasks;
+
+public class WordsReverser {
+    public static String reverseWords(final String original) {
+        String result = "";
+        String[] words = original.split(" ");
+        for(String word : words) {
+            result += reverseWord(word) + " ";
+        }
+
+        return result.substring(0, result.length() - 1); // Remain the last character - " "
+    }
+
+    private static String reverseWord(final String word) {
+        String reversedWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+
+        return reversedWord;
+    }
+}
